9ee55f8ed17d18c538f814686a7f27fef18c0c68
stalfos1211 <stalfos1211@gmail.com>
2016-05-30 10:14:17 -0500

    working communication between background and pop-up

--- Chrome Extensions/Template Detect/src/bg/background.js
+++ Chrome Extensions/Template Detect/src/bg/background.js
@@ -6,13 +6,13 @@
 
 
 //example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
+/*chrome.extension.onMessage.addListener(
   function(request, sender, sendResponse) {
   	chrome.pageAction.show(sender.tab.id);
     sendResponse();
   });
 
-var currentUrl = '';
+var currentUrl = '';*/
 
 /*chrome.webNavigation.onCompleted.addListener(function(e) {
 	var currentUrl = (e.url == 'about:blank') ? currentUrl : e.url;
@@ -37,12 +37,29 @@ var currentUrl = '';
 	});
 });
 */
+
+
+
+console.log("I am background.js");
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.greeting == "hello")
+      sendResponse({
+        msg: "goodbye!"
+      });
+  });
+
+
+var currentUrl =  '';
+
 chrome.tabs.onUpdated.addListener(function(tabId , info) {
     if (info.status == "complete") {
 
-        console.log(info)
+        //console.log(info)
         chrome.tabs.getSelected(null,function(tab) {
         	var tablink = tab.url;
+        	currentUrl = tablink;
 	    	console.log('onUpdated',tablink)
 	    });
     }
@@ -50,10 +67,39 @@ chrome.tabs.onUpdated.addListener(function(tabId , info) {
 
 chrome.tabs.onActivated.addListener(function(tabId , info) {
 
-        //console.log(tabId)
+        //console.log('tabId',tabId)
         //console.log(info)
         chrome.tabs.getSelected(null,function(tab) {
         	var tablink = tab.url;
 	    	console.log('onActivated',tablink)
+	    	currentUrl = tablink;
+	    	chrome.tabs.sendMessage(tabId.tabId, {"message": "tab_activated"});
+	    	console.log('test', document.getElementById('#copy-id'))
 	    });
 });
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  // Send a message to the active tab
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var activeTab = tabs[0];
+    
+    chrome.tabs.sendMessage(tabId.tabId, {"message": "tab_activated"});
+
+    //sendVarsToPopup();
+
+    console.log('BG CLICKED')
+
+  });
+})
+
+/*function sendVarsToPopup(){
+	var popups = chrome.extension.getViews({type: "popup"});
+	if (0 < popups.length) {
+		var test popups[0].link = 42;
+	}
+}*/
+
+/*var popups = chrome.extension.getViews({type: "popup"});
+if (0 < popups.length) {
+ var test popups[0].variable = 42;
+}*/
---
+++ Chrome Extensions/Template Detect/src/browser_action/browser_action.js
@@ -0,0 +1,59 @@
+//alert('working')
+
+/*var btn = document.getElementById('copy-id');
+
+btn.addEventListener("click", btnClick);
+
+function btnClick(){
+
+	//alert('button test');
+
+	try {
+    	alert($);
+	}
+	catch(err) {
+	    document.getElementById("demo").innerHTML = err.message;
+	}
+}*/
+
+var link = '';
+
+console.log("I am popup.js");
+
+document.getElementById("copy-id").onclick = function(e){
+  hello();
+}
+
+function hello() {
+  console.log("hello");
+  chrome.runtime.sendMessage({
+      greeting: "hello"
+    },
+    function(response) {
+      document.getElementById("template-name").textContent = response.msg;
+    });
+}
+
+
+
+chrome.browserAction.onClicked.addListener(
+  function (tabs) {
+    alert('test alert from popup');
+    console.log('browserAction a:', tabs);
+})
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+  	console.log('message recieved')
+    if( request.message === "tab_activated") {
+    	console.log('message recieved tab has been activated');
+    	alert('message recieved tab has been activated');
+    }
+
+});
+
+$('#template-name').text('this is a test');
+console = chrome.extension.getBackgroundPage().console
+//alert(chrome.extension.getBackgroundPage().link)
+
+//chrome.extension.getBackgroundPage().variable = 42;
