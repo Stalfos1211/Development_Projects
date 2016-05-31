ffeb67696a651f6b1c1230288eecaea70eb4ac6c
stalfos1211 <stalfos1211@gmail.com>
2016-05-31 18:47:30 -0500

    remove unneccessary files updated templateList added window change listener

--- Chrome Extensions/Template Detect/src/bg/background.js
+++ Chrome Extensions/Template Detect/src/bg/background.js
@@ -5,35 +5,56 @@ var templateInfo = {};
 // When tab finish loading update the current url
 chrome.tabs.onUpdated.addListener(function(tabId , info) {
     if (info.status == "complete") {
-
-        chrome.tabs.getSelected(null,function(tab) {
-        	var currentUrl = tab.url;
-        	matchUrl(currentUrl);
-	    });
+        updateInfo();
     }
 });
 
 // When switching tabs update current url
 chrome.tabs.onActivated.addListener(function(tabId , info) {
-
-        chrome.tabs.getSelected(null,function(tab) {
-        	var currentUrl = tab.url;
-	    	matchUrl(currentUrl);
-	    });
+        updateInfo();
 });
 
+// When switching windows update current url
+chrome.windows.onFocusChanged.addListener(function(windowId) {
+	updateInfo();
+})
+
+
+function updateInfo() {
+	chrome.tabs.getSelected(null,function(tab) {
+    	var currentUrl = tab.url;
+    	matchUrl(currentUrl);
+    });
+}
 
 function matchUrl(url) {
 
 	// Test match each item on the template list
 	for (var i=0; i < templateList.length; i++){
+
+		// Initial check if match
 		if (url.indexOf(templateList[i].urlToMatch) > -1) {
 
-			iconEnabled(true);
+			// Check if extra strings for matching exist
+			if(templateList[i].contains) {
 
-			templateInfo = {"name" : templateList[i].templateName, "id" : getId(url, templateList[i].findIdText)};
-			
-			return;
+				// Extra check if match
+				if(url.indexOf(templateList[i].contains))  {
+					iconEnabled(true);
+
+					templateInfo = {"name" : templateList[i].templateName, "id" : getId(url, templateList[i].findIdText)};
+					
+					return;
+				}
+			}
+			else {
+				iconEnabled(true);
+
+				templateInfo = {"name" : templateList[i].templateName, "id" : getId(url, templateList[i].findIdText)};
+				
+				return;
+
+			}
 		} 
 		else {
 			iconEnabled(false);
@@ -55,20 +76,97 @@ function iconEnabled(foundMatch) {
 var templateList = [
 
 	{
-	"templateName" : "Resident Portal",
-	"urlToMatch" : "residentportal.com/resident_portal/?module=authentication&action=view_login",
+	"templateName" : "Municipal Online Payments",
+	"urlToMatch" : "https://www.municipalonlinepayments.com/",
+	"findIdText" : [["municipalonlinepayments.com/", "/login"]],
+	"contains" : "/login"
+	},
+	{
+	"templateName" : "Appfolio",
+	"urlToMatch" : ".appfolio.com/connect/users/sign_in",
+	"findIdText" : [["https://", ".appfolio.com/connect/users/sign_in"]]
+	},
+	{
+	"templateName" : "Smartclick",
+	"urlToMatch" : "https://smartclick.campusapts.com/Login.aspx"
+	},
+	{
+	"templateName" : "Realpage Welcomehome",
+	"urlToMatch" : "https://property.onesite.realpage.com/welcomehome",
+	"findIdText" : [["welcomehome/?siteid=", ".#url=%23login"]],
+	"contains" : "#url=%23login"
+	},
+	{
+	// need id for sign up
+	"templateName" : "Realpage Activebuilding",
+	"urlToMatch" : "https://login.activebuilding.com/auth/login"
+	},
+	{
+	// for testing https://property.onesite.realpage.com/templates/template_concept03/login/login.asp?w=havenoaksapts
+	"templateName" : "Realpage ResidentPortal",
+	"urlToMatch" : "https://property.onesite.realpage.com/templates/",
+	"findIdText" : [["templates/", "/login/"],["/login/login.asp?w="]],
+	"contains" : "/login/login.asp?w="
+	},
+	{
+	"templateName" : "Rentcafe SecureCafe",
+	"urlToMatch" : ".securecafe.com/residentservices/",
+	"findIdText" : [["https://", ".securecafe.com/residentservices/"],[".securecafe.com/residentservices/", "/userlogin.aspx"]],
+	"contains" : "/userlogin.aspx"
+	},
+	{
+	"templateName" : "Rentcafe Resident Services",
+	"urlToMatch" : "https://www.rentcafe.com/residentservices/",
+	"findIdText" : [["https://", "/userlogin.aspx"]],
+	"contains" : "/userlogin.aspx"
+	},
+	{
+	"templateName" : "Property Solutions",
+	"urlToMatch" : ".residentportal.com/resident_portal/?module=authentication",
 	"findIdText" : [["https://", ".residentportal.com/"]]
 	},
 	{
-	"templateName" : "Secure Cafe",
-	"urlToMatch" : "securecafe.com/residentservices/",
-	"findIdText" : [["https://", ".securecafe"], ["residentservices/", "/userlogin.aspx"]]
+	"templateName" : "Paylease",
+	"urlToMatch" : "https://www.paylease.com/login"
 	},
 	{
-	"templateName" : "RentPayment",
-	"urlToMatch" : "https://www.rentpayment.com/pay/login.html",
-	"findIdText" : [["pay/login.html?pc="]]
-	}/*,
+	//need id for sign up
+	"templateName" : "Yapstone",
+	"urlToMatch" : "https://www.rentpayment.com/pay/login.html"
+	},
+	{
+	"templateName" : "Aptexx",
+	"urlToMatch" : ".aptexx.com/auth/index/cid/",
+	"findIdText" : [["https://", ".aptexx.com/"], ["index/cid/"]]
+	},
+	{
+	"templateName" : "Smartclick",
+	"urlToMatch" : "https://smartclick.campusapts.com/Login.aspx"
+	},
+	{
+	"templateName" : "Student Housing Cafe",
+	"urlToMatch" : "https://www.studenthousingcafe.com/residentservices/",
+	"contains" : "/userlogin.aspx",
+	"findIdText" : [["residentservices/", "/userlogin.aspx"]]
+	},
+	{
+	"templateName" : "Smarthub",
+	"urlToMatch" : ".smarthub.coop/Login.html",
+	"findIdText" : [["https://", ".smarthub"]]
+	},
+	{
+	"templateName" : "Convergentcare",
+	"urlToMatch" : ".convergentcare.com/",
+	"contains" : "/goToLogin.action",
+	"findIdText" : [["https://", ".convergentcare"]]
+	},
+	{
+	"templateName" : "OSCP",
+	"urlToMatch" : "https://billing",
+	"contains" : "/oscp/",
+	"findIdText" : [["billing.", "/oscp/"]]
+	}
+	/*,
 	{
 	"templateName" : "Google",
 	"urlToMatch" : "google.com"
@@ -102,7 +200,8 @@ function getId(url, findIdText) {
 			else {
 				var beginningtext = findIdText[j][0];
 				var beginning = url.indexOf(beginningtext) + beginningtext.length;
-				id = url.substr( beginning, url.length );
+				if (j>0){id += ','};
+				id += url.substr( beginning, url.length );
 			}
 		}
 
--- Chrome Extensions/Template Detect/src/browser_action/browser_action.js
+++ Chrome Extensions/Template Detect/src/browser_action/browser_action.js
@@ -1,14 +1,15 @@
 
 var id = '';
 
-chrome.browserAction.onClicked.addListener(getTemplateInfo());
+if (chrome.browserAction) {
+  chrome.browserAction.onClicked.addListener(getTemplateInfo());
+}
 
 function getTemplateInfo(){
   chrome.runtime.sendMessage({
       test: "get"
     },
     function(response) {
-      console.log('response', response.templateInfo);
       document.getElementById("template-name").textContent = response.templateInfo.name;
       document.getElementById("template-id").textContent = response.templateInfo.id;
       id = response.templateInfo.id;
